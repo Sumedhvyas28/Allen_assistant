1df4765abdb3a4154de0c94070dd370728043d99
sumedhvyas <sumedhvyas3@gmail.com>
2024-06-17 14:06:30 +0530

    second commit-mic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:allen/feature_list.dart';
 import 'package:allen/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,12 +12,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // app bar - nav
       appBar: AppBar(
         title: const Text('Allen'),
-        leading: Icon(Icons.menu),
+        leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
       body: Column(
@@ -54,8 +93,93 @@ class _MyHomePageState extends State<MyHomePage> {
               horizontal: 20,
               vertical: 10,
             ),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Pallete.borderColor,
+              ),
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Good Morning , what task can I do for you ?',
+                style: TextStyle(
+                  color: Pallete.mainFontColor,
+                  fontFamily: 'Cera Pro',
+                  fontSize: 25,
+                ),
+              ),
+            ),
           ),
+
+          // 1 line text
+
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            child: const Text(
+              'Here are a few features.',
+              style: TextStyle(
+                fontFamily: 'Cera Pro',
+                fontWeight: FontWeight.bold,
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+              ),
+            ),
+          ),
+
+          // features List ---
+          const Column(
+            children: [
+              FeatureList(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                descriptionText:
+                    'A smarter way to stay organized and informed with ChatGPT',
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              FeatureList(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Get inspired and stay creative with your personal assistant powered by Dall - E ',
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              FeatureList(
+                color: Pallete.thirdSuggestionBoxColor,
+                headerText: 'Smart Voice Assistant',
+                descriptionText:
+                    'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              )
+            ],
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            startListening();
+          } else if (speechToText.isListening) {
+            stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        child: const Icon(Icons.mic),
       ),
     );
   }
